0aa1d445b6ef7a356211f9f313b6227db226cd0a
Tamara Suslova <tamara.suslova@gmail.com>
2024-02-01 10:45:15 +0700

    Basic films table view

--- KinopoiskClient/KinopoiskClient/FilmsList/FilmsListViewController.swift
+++ KinopoiskClient/KinopoiskClient/FilmsList/FilmsListViewController.swift
@@ -6,11 +6,74 @@
 //
 
 import UIKit
+import Combine
 
-final class FilmsListViewController: UIViewController {
+final class FilmsListViewController: UITableViewController {
     private var listViewModel = FilmsListViewModel()
-    
+    private var bindings = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func bindViewModelToView() {
+        listViewModel.$films
+            .receive(on: RunLoop.main)
+            .sink(receiveValue: { [weak self] _ in
+                self?.tableView.reloadData()
+            })
+            .store(in: &bindings)
+        
+        let stateValueHandler: (ListViewModelState) -> Void = { [weak self] state in
+            switch state {
+            case .loading:
+                self?.startLoading()
+            case .loadingFinished:
+                self?.finishLoading()
+            case .error:
+                //TODO
+                self?.finishLoading()
+            }
+        }
+        
+        listViewModel.$state
+            .receive(on: RunLoop.main)
+            .sink(receiveValue: stateValueHandler)
+            .store(in: &bindings)
+    }
+    
+    private func startLoading() {
+        refreshControl?.beginRefreshing()
+    }
+    
+    private func finishLoading() {
+        refreshControl?.endRefreshing()
+        tableView.reloadData()
+    }
+
+}
+
+//MARK: - IBActions
+extension FilmsListViewController {
+    @IBAction func refreshList(_ sender: Any) {
+        startLoading()
+    }
+}
+
+//MARK: - TableView Datasource
+extension FilmsListViewController {
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO: binding
+        return listViewModel.films.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //TODO: initialization with cell view model
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: FilmCellView.identifier,
+            for: indexPath) as! FilmCellView
+        cell.viewModel = FilmCellViewModel()
+        return cell
     }
 }
--- KinopoiskClient/KinopoiskClient/FilmsList/FilmsListViewModel.swift
+++ KinopoiskClient/KinopoiskClient/FilmsList/FilmsListViewModel.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+enum ListViewModelState {
+    case loading
+    case loadingFinished
+    case error
+}
+
 final class FilmsListViewModel {
-    
+    @Published private(set) var films: [Film] = FilmsListViewModel.dummyFilmsList()
+    @Published private(set) var state: ListViewModelState = .loadingFinished
+}
+ 
+//Temporary test data
+extension FilmsListViewModel {
+    static func dummyFilmsList() -> [Film] {
+        [Film(kinopoiskId: 0, nameRu: "Анна детективъ", posterUrl: "", posterUrlPreview: "")]
+    }
 }
