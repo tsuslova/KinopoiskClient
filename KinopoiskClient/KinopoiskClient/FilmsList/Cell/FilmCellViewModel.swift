b7ba9db53f716e28b7423ee5d4627a4fbc1a5102
Tamara Suslova <tamara.suslova@gmail.com>
2024-02-01 14:37:20 +0700

    show film description

--- KinopoiskClient/KinopoiskClient/FilmsList/Cell/FilmCellViewModel.swift
+++ KinopoiskClient/KinopoiskClient/FilmsList/Cell/FilmCellViewModel.swift
@@ -23,7 +23,30 @@ final class FilmCellViewModel {
     
     private func setUpBindings() {
         title = film.nameRu ?? "Unnamed movie"
-        //TODO: description, image
+        fillDescription()
+        //TODO: image
     }
+    
+    private func fillDescription() {
+        let countries = film.countries.shortenTo3
+        let genres = film.genres.shortenTo3
+        
+        if !countries.isEmpty && !genres.isEmpty {
+            description = "\(countries), \(genres), \(film.year)"
+        } else if !countries.isEmpty && genres.isEmpty{
+            description = "\(countries), \(film.year)"
+        } else if countries.isEmpty && !genres.isEmpty{
+            description = "\(genres), \(film.year)"
+        } else {
+            description = "\(film.year)"
+        }
+    }
+}
 
+private extension Array where Element == String {
+    var shortenTo3: String {
+        self.count > 3 ?
+        "\(self[0..<3].joined(separator: ", "))[...]" :
+        "\(self.joined(separator: ", "))"
+    }
 }
