c1c2c1e9a1fefb9a02cc36d2577a4fed7f1cd679
Tamara Suslova <tamara.suslova@gmail.com>
2024-02-16 16:04:12 +0700

    remove unused code

--- KinopoiskClient/KinopoiskClient/DataService/HTTP/HTTPClient.swift
+++ KinopoiskClient/KinopoiskClient/DataService/HTTP/HTTPClient.swift
@@ -6,17 +6,7 @@
 //
 
 import Foundation
-
-//Wrapper for URLSessionDataTask to reduce dependencies from URLSession library
-//from client's code
-public protocol Task {
-    func cancel()
-    func resume()
-}
-
 public protocol HTTPClient {
     func dataTaskPublisher(for url: URL) -> URLSession.DataTaskPublisher?
     func dataTaskPublisher(for url: URL, parameters: [String: Any]) -> URLSession.DataTaskPublisher?
 }
-
-extension URLSessionDataTask: Task {}
